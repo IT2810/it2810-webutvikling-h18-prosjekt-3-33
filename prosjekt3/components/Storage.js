b75b864227b9117fcb0a005ab1f2bff01d1331b7
ulrikah <ulrikhalmoy@gmail.com>
2018-09-30 15:46:12 +0200

    Start working on the Storage component #4

---
+++ prosjekt3/components/Storage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    View,
+    Text
+    AsyncStorage,
+} from 'react-native';
+
+
+// class for getting and setting data in the AsyncStorage
+export default class Storage extends React.Component {
+
+    /* ex.:
+      key = 'user'
+      obj =  
+      {
+            name: 'Jørgen Stai',
+            email: 'test@gmail.com',
+            city: 'Stockholm',
+        }
+    */
+    saveData(key, obj) {
+        let obj = JSON.stringify(obj);
+        AsyncStorage.setItem(key, JSON.stringify(obj));
+    }
+
+
+    /* ex.:
+
+      key   = 'user'
+      item  = 'name'
+
+    */
+    displayData = async (key, item) => {
+      try {
+        let obj = await AsyncStorage.getItem(key);
+        let parsed = JSON.parse(obj);
+        alert(parsed.item); // parsed.item returerer det man fetcher etter, f.eks. 'name' under nøkkelen 'user'
+      }
+
+      catch(error) {
+        alert(error);
+      }
+    }
+}
+  
