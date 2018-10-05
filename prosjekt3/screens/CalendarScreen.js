9bd0b9d7187107cfaa19a5ba56217b06c7a82959
Jørgen Stamnes <jorgen@jorgen.com>
2018-10-05 17:47:44 +0200

    Start experiment on implement calenders for PIMM

--- prosjekt3/screens/CalendarScreen.js
+++ prosjekt3/screens/CalendarScreen.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {
 	View,
   	Text,
-  	Button,
-  	AsyncStorage,
+  	Button, ScrollView,
+  	AsyncStorage, StyleSheet
 } from 'react-native';
 import * as Storage from '../components/Storage.js'
+import { Calendar, CalendarList, Agenda, calendarTheme} from 'react-native-calendars';
 
 export default class CalendarScreen extends React.Component {
 	constructor(props){
@@ -22,12 +23,34 @@ export default class CalendarScreen extends React.Component {
   	render() {
 
 		return (
-	    	<View>
-	    		<Text> Calendar </Text>
-	    	</View>
+			<ScrollView>
+			<Agenda
+			items ={
+				{'2018-10-01': [{text: 'Morning appointment'},{text: 'Midday appointment'},{text: 'Afternoon appointment'}],
+				 '2018-10-02': [{text: 'item 2 - any js object'}],
+				 '2018-10-03': [],
+				 '2018-10-04': [{text: 'item 3 - any js object'},{text: 'any js object'}],
+				}}
+			
+			renderItem={(item,firstItemInDay) => {return (<View>
+														<Text style = {styles.item}>{firstItemInDay}</Text>
+														<Text style = {styles.item}>{item.key}</Text>
+														</View>)}}
+			renderEmptyDate={() => {return (<View><Text></Text></View>);}}
+			rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+			/>
+			</ScrollView>
 	    )
   	}
 }
 
+
+const styles = StyleSheet.create({
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    }
+  }); 
 
 
